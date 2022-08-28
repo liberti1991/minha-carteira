9260f11162f1024aabfdf8db1d29626de097ae98
rodrigo liberti <liberti1991@gmail.com>
2022-08-27 22:17:17 -0300

    ajuste grafic two

--- src/assets/components/GraficTwo.tsx
+++ src/assets/components/GraficTwo.tsx
@@ -15,22 +15,31 @@ interface IGraficTwo {
 export const GraficTwo: React.FC<IGraficTwo> = ({ data, lineColorAmountEntry, lineColorAmountOutput }) => (
   <Container>
     <h2>Histórico de saldo</h2>
-    <ResponsiveContainer>
-      <LineChart data={data} margin={{ top: 5, right: 20, left: 20, bottom: 5 }}>
-        <CartesianGrid strokeDasharray=" 3 3 " stroke="#cecece" />
-        <XAxis dataKey="month" stroke="#cecece" />
-        <Tooltip />
-        <Line type="monotone" dataKey="amountEntry" name="Entradas" stroke={lineColorAmountEntry} strokeWidth={5} dot={{ r: 5 }} activeDot={{ r: 8 }} />
-        <Line type="monotone" dataKey="amountOutput" name="Saídas" stroke={lineColorAmountOutput} strokeWidth={5} dot={{ r: 5 }} activeDot={{ r: 8 }} />
-      </LineChart>
-    </ResponsiveContainer>
+    <div>
+      <ResponsiveContainer>
+        <LineChart data={data} margin={{ top: 5, right: 20, left: 20, bottom: 5 }}>
+          <CartesianGrid strokeDasharray=" 3 3 " stroke="#cecece" />
+          <XAxis dataKey="month" stroke="#cecece" />
+          <Tooltip />
+          <Line type="monotone" dataKey="amountEntry" name="Entradas" stroke={lineColorAmountEntry} strokeWidth={5} dot={{ r: 5 }} activeDot={{ r: 8 }} />
+          <Line type="monotone" dataKey="amountOutput" name="Saídas" stroke={lineColorAmountOutput} strokeWidth={5} dot={{ r: 5 }} activeDot={{ r: 8 }} />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
   </Container>
 );
 
 const Container = styled.section`
-  height: 340px;
   background-color: ${(props) => props.theme.colors.tertiary};
   color: ${(props) => props.theme.colors.white};
   border-radius: 10px;
   padding: 30px 20px;
+
+  > h2 {
+    margin-bottom: 20px;
+  }
+
+  > div {
+    height: 240px;
+  }
 `;
