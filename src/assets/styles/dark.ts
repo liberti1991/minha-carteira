fc15864d12191e4af42db7da4ac7b6ab3279f1ff
rodrigo liberti <liberti1991@gmail.com>
2022-08-29 17:22:10 -0300

    paleta cores

--- src/assets/styles/dark.ts
+++ src/assets/styles/dark.ts
@@ -2,9 +2,9 @@ export const dark = {
   title: 'dark',
 
   colors: {
-    primary: '#1B1F38',
-    secondary: '#252A48',
-    tertiary: '#313862',
+    primary: '#000',
+    secondary: '#1C1C1C',
+    tertiary: '#1B1F38',
 
     white: '#FFF',
     black: '#000',
@@ -15,3 +15,21 @@ export const dark = {
     warning: '#E44C4E',
   },
 };
+
+// export const dark = {
+//   title: 'dark',
+
+//   colors: {
+//     primary: '#1B1F38',
+//     secondary: '#252A48',
+//     tertiary: '#313862',
+
+//     white: '#FFF',
+//     black: '#000',
+//     gray: '#BFBFBF',
+
+//     success: '#4E41F0',
+//     info: '#F7931B',
+//     warning: '#E44C4E',
+//   },
+// };
--- src/assets/styles/light.ts
+++ src/assets/styles/light.ts
@@ -3,7 +3,7 @@ export const light = {
 
   colors: {
     primary: '#DCDCDC',
-    secondary: '#FFFFFF',
+    secondary: '#BFBFBF60',
     tertiary: '#F5F5F5',
 
     white: '#000',
@@ -15,3 +15,21 @@ export const light = {
     warning: '#FF6961',
   },
 };
+
+// export const light = {
+//   title: 'light',
+
+//   colors: {
+//     primary: '#DCDCDC',
+//     secondary: '#Ffffff',
+//     tertiary: '#F5F5F5',
+
+//     white: '#000',
+//     black: '#FFF',
+//     gray: '#BFBFBF',
+    
+//     success: '#03BB85',
+//     info: '#3B5998',
+//     warning: '#FF6961',
+//   },
+// };
